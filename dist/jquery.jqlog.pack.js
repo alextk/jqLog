2a833a9faf42c2f662140de3649ea3e570b47b0d
Alex Tkachev <tkachev.alex@gmail.com>
2011-07-28 17:48:32 +0300

    commiting files for release of version 0.0.2

--- dist/jquery.jqlog.pack.js
+++ dist/jquery.jqlog.pack.js
@@ -2,14 +2,14 @@
 * jqLog - jQuery logging for javascript
 *
 * Version: 0.0.2
-* Build: 48
+* Build: 49
 * Copyright 2011 Alex Tkachev
 *
 * Dual licensed under MIT or GPLv2 licenses
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: 28/07/2011 17:45:15
+* Date: 28/07/2011 17:48:30
 */
 
 eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(8(a){a.9={g:{}}})(q),8(a){a.9.v={1f:1,10:2,1g:3,1h:4,1i:5,1j:6,S:8(b,c){s(a.1d(c)==="1k")h b<c;h!1}}}(q),8(a){a.9.g.F=8(){7.k.x(7,t)},a.n(a.9.g.F.l,{k:8(b){a.n(7,b)},I:8(a,b,c){s(7.9.R(7,a)){s(c)X(j d 15 c)b=b.1l("{"+d+"}",c[d]);7.9.J(7,a,b)}}}),["Q","L","H","K","E","M"].A(8(b){a.9.g.F.l[b]=8(){1m.l.1n.1o(t,a.9.v[b.12()]),7.I.x(7,t)}})}(q),8(a){a.9.g.Y=8(){7.k.x(7,t)},a.n(a.9.g.Y.l,{u:m,p:m,k:8(a){7.9=a,7.P()},P:8(){7.u={},7.p=7.u.r=y a.9.g.F({9:7.9,16:"",N:[],i:a.9.v.10})},B:8(a){a.O&&(7.p.i=a.O.i);X(j b 15 a)s(b!="O"){j c=a[b],d=7.z(b);c.i&&(d.i=c.i)}},z:8(b){s(!b||b.17===0)h 7.p;j c=b.1p("."),d=7.u;c.A(8(a){d[a]||(d[a]={r:m}),d=d[a]}),d.r===m&&(d.r=y a.9.g.F({9:7.9,16:b,N:c}));h d.r},R:8(b,c){s(a.9.v.S(c,7.p.i)||a.9.v.S(c,b.i))h!1;j d=7.u;X(j e=0;e<b.N.17;e++){j f=b.N[e];s(d[f].r&&d[f].r.i&&a.9.v.S(c,d[f].r.i))h!1;d=d[f]}h!0}})}(q),8(a){a.9.g.T=8(){7.k.x(7,t)},a.n(a.9.g.T.l,{W:!0,k:8(){7.11=[]},I:8(a,b){7[a](b)}}),["Q","L","H","K","E","M"].A(8(b){a.9.g.T.l[b]=8(a){7.11.19({1a:b,U:a})}})}(q),8(a){a.9.g.C=8(){7.k.x(7,t)},a.n(a.9.g.C.l,{k:8(){7.o=o,7.W=1b 7.o!=="1c"&&7.o!==m},I:8(a,b){7[a](b)}}),["Q","L","H","K","E"].A(8(b){a.9.g.C.l[b]=8(a){7.o[b](a)}}),a.9.g.C.l.M=a.9.g.C.l.E}(q),8(a){a.9.g.G=8(){7.k.x(7,t)};j b=a.9.v,c={};["Q","L","H","K","E","M"].A(8(a){c[b[a.12()]]=a}),a.9.g.G.14=8(a){h c[a]},a.n(a.9.g.G.l,{o:m,k:8(b){a.n(7,b),7.o||(7.o=y a.9.g.C)},J:8(b){s(!7.o.W)h!1;j c=a.9.g.G.14(b.i);7.o[c](b.U);h!0}})}(q),8(a){a.9.g.V=8(){7.k.x(7,t)},a.n(a.9.g.V.l,{D:m,w:m,p:m,Z:m,k:8(a){7.Z=a,7.P()},P:8(){7.D=y a.9.g.Y(7),7.p=7.D.p,7.w=[],7.B(7.Z)},R:8(a,b){h 7.D.R(a,b)},J:8(a,b,c){j d={1q:y 18,z:a,i:b,U:c};7.w.A(8(a){a.J(d)})},z:8(a){h 7.D.z(a)},B:8(a){a.u&&7.D.B(a.u),a.w&&(7.w=a.w)}}),a.n(a.9,{13:{w:[y a.9.g.G],u:{O:{i:a.9.v.10}}}});j b=8(){j b=y a.9.g.V(a.9.13);h{1e:b,p:8(){h b.p},z:8(a){h b.z(a)},B:8(a){b.B(a)}}}();a.n(a.9,b)}(q)',62,89,'|||||||this|function|jqLog|||||||classes|return|level|var|initialize|prototype|null|extend|console|rootLogger|jQuery|_logger|if|arguments|loggers|Level|appenders|apply|new|logger|each|configure|BrowserConsole|manager|error|Logger|ConsoleAppender|info|log|doAppend|warn|debug|fatal|nameArr|root|reset|trace|isLevelEnabled|isLower|ArrayConsole|message|JQLog|enabled|for|LoggersManager|defaultConfig|DEBUG|buffer|toUpperCase|defaults|loggingMethodForLevel|in|name|length|Date|push|method|typeof|undefined|type|_instance|TRACE|INFO|WARN|ERROR|FATAL|number|replace|Array|unshift|call|split|date'.split('|'),0,{}))
