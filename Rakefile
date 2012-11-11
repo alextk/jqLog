bd010de36f62d0fad30dd647ae055823289c3e82
Alex <tkachev.alex@gmail.com>
2012-11-11 14:47:11 +0200

    include ajax appender in pack

--- Rakefile
+++ Rakefile
@@ -8,6 +8,6 @@ project = JsProjectBuilder.new(
   :name => 'jqLog',
   :description => 'jQuery logging for javascript',
   :file_name => 'jquery.jqlog.js',
-  :js_files => %w{base.js level.js layouter.js logger.js loggers-manager.js array-console.js browser-console.js console-appender.js jqlog.js}
+  :js_files => %w{base.js level.js layouter.js logger.js loggers-manager.js array-console.js browser-console.js console-appender.js ajax-appender.js jqlog.js}
 )
 JsProjectBuilder::Tasks.new(project)
--- dist/jquery.jqlog.js
+++ dist/jquery.jqlog.js
@@ -2,14 +2,14 @@
 * jqLog - jQuery logging for javascript
 *
 * Version: 0.0.3
-* Build: 89
+* Build: 90
 * Copyright 2011 Alex Tkachev
 *
 * Dual licensed under MIT or GPLv2 licenses
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: 11 Nov 2012 14:09:11
+* Date: 11 Nov 2012 14:42:50
 */
 
 (function($) {
@@ -285,6 +285,76 @@
       var msg = this.getLayouter().eventToString(event);
       this.console[loggingMethod](msg); //perform actual log
       return true;
+    }
+
+  });
+
+})(jQuery);
+(function($) {
+
+  $.jqLog.classes.AjaxAppender = function() {
+    this.initialize.apply(this, arguments);
+  };
+
+  $.extend($.jqLog.classes.AjaxAppender.prototype, {
+
+    url: null, //url to which logger statements will be posted
+    flushSize: 100, //messages will be sent when buffer is full (has 100 messages)
+    bufferLocked: false, //if buffer is locked after ajax request by slicing the buffer
+
+    initialize: function(config) {
+      if(!config.url) throw "config.url must be present in ajax appender config"
+      $.extend(this, config);
+
+      this.buffer = [];
+    },
+
+    getLayouter: function(){
+      return this.layouter || $.jqLog.rootLayouter();
+    },
+
+    doAppend: function(event){
+      var msg = this.getLayouter().eventToString(event);
+      this._safePushToBuffer(msg);
+      if(this.buffer.length >= this.flushSize) this.flush();
+      return true;
+    },
+
+    _safePushToBuffer: function(msg){
+      var self = this;
+      if(this.bufferLocked){
+        window.setTimeout(5, function(){ self._safePushToBuffer(msg); });
+      } else {
+        this.buffer.push(msg);
+      }
+    },
+
+    // send all messages in buffer to server
+    flush: function(){
+      var self = this;
+      if(this.flushing) return;  //do not send multiple requests
+
+      this.flushing = true;
+      var messages = this.buffer.clone(); //we clone it since buffer might continue to fill up as we sending ajax request
+      $.ajax({
+        url: this.url,
+        type: 'post',
+        data: $.param({log_messages: messages}),
+        success: function(){
+          this.bufferLocked = true;
+          try{
+            self.buffer = self.buffer.slice(messages.length);
+          }catch(e){
+          }finally{
+            this.bufferLocked = false;
+          }
+        },
+        error: function(xhr){
+        },
+        complete: function(){
+          self.flushing = false;
+        }
+      });
     }
 
   });
